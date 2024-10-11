b15035de818f3cec346c1578e8b229f5528942cc
rezaul-web <rmdrezaul75@gmail.com>
2024-10-12 00:57:45 +0530

    completed authentication

---
+++ app/src/main/java/com/example/chatapp/MainAppComposable.kt
@@ -0,0 +1,35 @@
+package com.example.chatapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.chatapp.features.signin.SignInScreen
+import com.example.chatapp.features.signup.SignUpScreen
+import com.example.chatapp.home.HomeScreen
+import com.google.firebase.auth.FirebaseAuth
+
+
+@Composable
+fun MainApp() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        val navController = rememberNavController()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val start = if (currentUser != null) "home" else "login"
+        NavHost(navController = navController, startDestination = start) {
+
+            composable("login") {
+                SignInScreen(navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController)
+            }
+            composable("home") {
+                HomeScreen(navController)
+            }
+        }
+    }
+}
